6c73ae04e802991c40763463f7c5b5ce5762b356
dxkien <114333374+dxkien@users.noreply.github.com>
2023-05-17 23:24:22 +0700

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TodoApp from './components/Todo-App';
+function App() {
+  return (
+     <div className="App">
+        <TodoApp></TodoApp>
+         </div>
+    
+  );
+}
+
+export default App;
